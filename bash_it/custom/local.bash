f01519001b31abf17d164035602feeb3ca1309ba
Nicholas Twyman <nick@truss.work>
2016-07-22 21:48:08 -0700

    Added OCaml init

--- bash_it/custom/local.bash
+++ bash_it/custom/local.bash
@@ -3,8 +3,14 @@
 # local.bash - Custom path settings etc
 #
 export GOPATH=$HOME/src/go_path
+
 pathmunge $GOPATH/bin after
 pathmunge /usr/local/sbin after
+
 if [ -d $HOME/.cargo/bin ]; then
     pathmunge $HOME/.cargo/bin
+fi
+
+if [ -d $HOME/.opam/opam-init/ ]; then
+   source /Users/nick/.opam/opam-init/init.sh > /dev/null 2> /dev/null
 fi
