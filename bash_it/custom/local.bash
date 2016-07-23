6a33a3e1e7789719a7695b0a2094fd946fd9e75a
Nick Twyman <nick@conky.org>
2016-07-22 21:49:45 -0700

    Added local go path

--- bash_it/custom/local.bash
+++ bash_it/custom/local.bash
@@ -2,9 +2,15 @@
 #
 # local.bash - Custom path settings etc
 #
+if [ -d /usr/local/go ]; then
+    pathmunge /usr/local/go/bin after
+fi
+
 export GOPATH=$HOME/src/go_path
 pathmunge $GOPATH/bin after
+
 pathmunge /usr/local/sbin after
+
 if [ -d $HOME/.cargo/bin ]; then
     pathmunge $HOME/.cargo/bin
 fi
