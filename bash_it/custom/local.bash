f3d660234a5ce39da9b5f92dbb6f7f0e51da32d2
Nicholas Twyman <nick@truss.work>
2017-11-20 12:20:45 -0800

    Added NVM and rbEnv hooks

--- bash_it/custom/local.bash
+++ bash_it/custom/local.bash
@@ -35,3 +35,13 @@ fi
 if [ -d $HOME/Library/Arcanist/ ]; then
    pathmunge $HOME/Library/Arcanist/arcanist/bin after
 fi
+
+if [ $(command -v rbenv) ]; then
+    eval "$(rbenv init -)"
+fi
+
+if [ -d "$HOME/.nvm" ]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
