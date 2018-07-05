acc2d44f3ea552f9dc28487c63197e30887d993b
Nick Twyman <nick@conky.org>
2018-07-05 09:22:19 -0700

    anaconda

--- bash_it/custom/local.bash
+++ bash_it/custom/local.bash
@@ -46,6 +46,10 @@ if [ $(command -v rbenv) ]; then
     eval "$(rbenv init -)"
 fi
 
+if [ -d $HOME/anaconda3/ ]; then
+    source $HOME/anaconda3/etc/profile.d/conda.sh
+fi
+
 if [ -d "$HOME/.nvm" ]; then
 	export NVM_DIR="$HOME/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
