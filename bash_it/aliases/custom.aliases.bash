3ac072e8e569422767a5c7bbe9e3a1c67f33a042
nickt truss <nick@truss.works>
2018-12-10 14:30:14 -0800

    goops

--- bash_it/aliases/custom.aliases.bash
+++ bash_it/aliases/custom.aliases.bash
@@ -9,12 +9,13 @@ alias E='SUDO_EDITOR="emacsclient" sudo -e'
 
 # And the truly custom stuff
 alias src='cd ~/src/'
-alias nrt='cd ~/src/analytics'
 alias ke='emacsclient -e '"'(kill-emacs)'"
+alias goops='git reset --soft HEAD~1'
 
 # Override some ones that don't work as expected
 alias gd='git diff'
 alias subl='open -a Sublime\ Text'
+
 
 # Add jp to pretty print json if we have jq 
 if [ `command -v jq` ]; then
--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -7,9 +7,43 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))))
+    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (csv-mode cider-hydra monokai-theme yaml-mode geiser company-anaconda anaconda-mode flycheck-ocaml merlin utop tuareg slime json-mode js2-mode gotest go-projectile go-eldoc company-go go-mode erlang rainbow-mode elisp-slime-nav cider clojure-mode rainbow-delimiters company helm-projectile helm smex ido-completing-read+ flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
